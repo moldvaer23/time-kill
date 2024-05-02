8097b67e61715cdea10b46419212af8eb9f67cd3
agro40tech <volkovvova67@gmail.com>
2024-05-02 11:59:03 +0400

    style(app): подключил шрифты и normalize

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,12 @@
 import React from 'react'
+
 import ReactDOM from 'react-dom/client'
+
 import App from './app/App'
 import reportWebVitals from './reportWebVitals'
+
+import './fonts/fonts.scss'
+import './styles/index.scss'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
